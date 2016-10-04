e69c89f89f91c3ec735dc581605eefea8cf0ce81
Andy Gill <andygill@ku.edu>
2016-10-04 10:05:09 -0500

    rotate through shaders.

--- hello-world/shader/shader-test.js
+++ hello-world/shader/shader-test.js
@@ -9,25 +9,25 @@ AFRAME.registerComponent('shader-test', {
 		keydown: this.onKeyDown.bind(this)
 	    };
 	    window.addEventListener('keydown', this.listeners.keydown, false);
-			this.state = false;
+			this.state = 0;
+			this.shaders = ["standard","flat"];
 	},
 	onKeyDown: function (event) {
 		var self = this;
 			if (event.code == "KeyT") {
 				console.log(event.code);
 				var f = function(o){ 
-					console.log(this);
-					if (self.state) {
-						this.setAttribute("material","shader","standard");
-					} else {
-						this.setAttribute("material","shader","flat");
-					}
+					console.log(this,self.shaders[self.state]);
+					this.setAttribute("material","shader",self.shaders[self.state]);
 				}
 				$("a-box").each(f);
 				$("a-sphere").each(f);
 				$("a-cylinder").each(f);
 				$("a-plane").each(f);
-				self.state = !self.state;
+				self.state++;
+				if (self.state >= self.shaders.length) {
+					self.state = 0;
+				}
 			}
 	}
 });
