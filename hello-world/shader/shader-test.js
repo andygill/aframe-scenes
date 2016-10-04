d04231a0ee44b7af6919352a3607a5c3197516e1
Andy Gill <andygill@ku.edu>
2016-10-04 10:01:06 -0500

    Got toggling of shader working.

--- hello-world/shader/shader-test.js
+++ hello-world/shader/shader-test.js
@@ -9,9 +9,25 @@ AFRAME.registerComponent('shader-test', {
 		keydown: this.onKeyDown.bind(this)
 	    };
 	    window.addEventListener('keydown', this.listeners.keydown, false);
+			this.state = false;
 	},
-
 	onKeyDown: function (event) {
-	    console.log(event.code);
+		var self = this;
+			if (event.code == "KeyT") {
+				console.log(event.code);
+				var f = function(o){ 
+					console.log(this);
+					if (self.state) {
+						this.setAttribute("material","shader","standard");
+					} else {
+						this.setAttribute("material","shader","flat");
+					}
+				}
+				$("a-box").each(f);
+				$("a-sphere").each(f);
+				$("a-cylinder").each(f);
+				$("a-plane").each(f);
+				self.state = !self.state;
+			}
 	}
-    });
+});
